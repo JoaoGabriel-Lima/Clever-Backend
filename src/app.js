34e81e64eea616a96431264f3b813a33851d4f46
João Lima <jg.limamarinho202@gmail.com>
2022-04-17 12:17:54 -0300

    Feat: Procfile

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(morgan("dev"));
 
 app.use((req, res, next) => {
   //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://clever-web.vercel.app");
   //Quais são os métodos que a conexão pode realizar na API
   res.header("Access-Control-Allow-Headers", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
